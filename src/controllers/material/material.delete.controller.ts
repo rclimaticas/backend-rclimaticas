08bbe928c0aabc22fac077b2662d5c203533e8c8
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-15 11:04:06 -0300

    :sparkles: feat: delete material

---
+++ src/controllers/material/material.delete.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import { prisma } from "../../utils/prisma";
+
+export class MaterialDeleteController {
+    async delete(req: Request, res: Response) {
+        const materialId = parseInt(req.params.id);
+        try {
+            await prisma.material.delete({ where: { id: materialId } });
+            res.json({ message: "Material excluído com sucesso." });
+        } catch (error) {
+            res.status(500).json({ error: "Erro ao excluir o material." });
+        }
+    }
+}
