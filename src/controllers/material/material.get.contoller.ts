1f0c18b201fc2421e75109b0bd60dabebf938af3
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-17 20:39:04 -0300

    :recycle: refactor: adding a general and filtered list of supporting materials

--- src/controllers/material/material.get.contoller.ts
+++ src/controllers/material/material.get.contoller.ts
@@ -4,7 +4,16 @@ import { prisma } from "../../utils/prisma";
 export class MaterialGetController {
     async index(req: Request, res: Response) {
         try {
-            const materials = await prisma.material.findMany();
+            const { media, topic, source, date} = req.query;
+            const materials = await prisma.material.findMany({
+                where: {
+                    // condições de filtro com base nos parâmetros de consulta
+                    media: media ? { contains: media.toString() } : undefined,
+                    topic: topic ? { contains: topic.toString() } : undefined,
+                    source: source ? { contains: source.toString() } : undefined,
+                    date: date ? new Date(date.toString()) : undefined,
+                }
+            });
             res.json(materials);
         } catch (error) {
             res.status(500).json({ error: "Erro ao buscar materiais." });
