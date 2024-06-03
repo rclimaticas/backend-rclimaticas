200a300c908253ec925c6c5ee65c10a2339cce9b
jvittor <102699222+jvittor@users.noreply.github.com>
2024-06-03 18:51:41 -0300

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import cors from 'cors';
 import bodyParser from 'body-parser';
 import multer from 'multer';
 
@@ -10,7 +9,6 @@ const upload = multer({
   limits: { fileSize: 50 * 1024 * 1024 } // Limite de 50MB para arquivos
 });
 
-app.use(cors()); // Adicionando o middleware cors
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(upload.single('image')); // Middleware para upload de arquivos
